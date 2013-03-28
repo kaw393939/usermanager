b298bab2f54010e1ba10d8a2da37fb6d592dc2ce
Bryan Nissen <Bryan@Webizly.com>
2013-03-28 19:47:44 -0400

    Account list and Details templates and rendering

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -12,7 +12,8 @@
 
     window.Controller = Backbone.Model.extend({
 		defaults: {
-			state: 'accounts'
+			state: 'accounts',
+			accountSelected: 0
 		},
 		
 		setState: function (state) {
@@ -21,7 +22,15 @@
 		
 		getState: function () {
 			return this.get('state');
-		}
+		},
+		
+		setAccountSelected: function (selected) {
+			this.set({accountSelected: selected})
+		},
+		
+		getAccountSelected: function () {
+			return this.get('accountSelected');
+				}
     });
 
     /////-----   COLLECTIONS   -----/////
@@ -44,6 +53,18 @@
             this.fetch({
                 update: true
             });
+        },
+        
+        search : function (id) {
+        	var _id;
+        	
+        	this.each(function(model){
+        		_id = model.get('uid');
+        		if(parseInt(id) == _id){
+        			return model;
+        		}
+        	});
+        	return null;
         }
     });
 
@@ -77,7 +98,6 @@
             
             changePage: function(){
             	var state = this.controller.getState();
-            	console.log('pew pew')
             	if (state == 'accounts'){
             		this.page = new window.AccountsView({
             			el: $('#appPage-inner'),
@@ -134,16 +154,61 @@
             template: Handlebars.compile($('#accountsView-template').html()),
 
             events: {
-
+				'click .accountView': 'clickAccount',
+				'mouseover .accountView': 'mouseOverAccount',
+				'mouseout .accountView': 'mouseOutAccount'
             },
 
             initialize: function () {
                 this.controller = this.options.controller;
+                
+                this.collection.bind('reset', this.render, this);
+                this.controller.bind('change:accountSelected', this.openAccount, this);
             },
 
             render: function () {
+            	var collection = this.collection,
+            		controller = this.controller,
+            		view;
                 $(this.el).html(this.template({}));
+                collection.each(function(account){
+                	view = new window.AccountView({
+                		el: $('#accountList'),
+                		model: account,
+                		controller: controller
+                	});
+                	view.render();
+                });
+                $('#sidebar').height($('#app').height() - parseInt($('#sidebar').css('padding-top')) - parseInt($('#sidebar').css('padding-bottom')))
+                $('#accountDetails').height($('#accountsBox').height() - 30);
                 return this;
+            },
+            
+            clickAccount: function(event){
+            	var id = $(event.currentTarget).attr('id');
+            	$('#'+this.controller.getAccountSelected()).removeClass('selected');
+            	$('#'+id).addClass('selected');
+            	this.controller.setAccountSelected(id);
+            },
+            
+            mouseOverAccount: function(event){
+            	var id = $(event.currentTarget).attr('id');
+            },
+            
+            mouseOutAccount: function (event){
+            	var id = $(event.currentTarget).attr('id');
+            },
+            
+            openAccount: function (){
+            	var id = this.controller.getAccountSelected(),
+            		account = this.collection.at(id),
+            		view = new window.AccountDetailsView({
+            			model: account,
+            			el: $('#accountDetails'),
+            			controller: this.controller
+            		});
+            		console.log(account)
+            	view.render();
             }
         });
 
@@ -151,7 +216,7 @@
             template: Handlebars.compile($('#accountView-template').html()),
 
             events: {
-
+			
             },
 
             initialize: function () {
@@ -159,20 +224,28 @@
             },
 
             render: function () {
-                $(this.el).html(this.template(this.model.toJSON()));
+                $(this.el).append(this.template(this.model.toJSON()));
                 return this;
             }
         });
 
         window.AccountDetailsView = Backbone.View.extend({
             template: Handlebars.compile($('#accountDetails-template').html()),
+            roleTemplate: Handlebars.compile($('#accountRole-template').html()),
 
             initialize: function () {
                 this.controller = this.options.controller;
             },
 
             render: function () {
+            	var roles = this.model.get('roles');
                 $(this.el).html(this.template(this.model.toJSON()));
+                for(var i = 0; i < roles.length; i++){
+                	$('.roles').append(this.roleTemplate({role: roles[i].role}));
+                	if(i == 0){
+                		$('.role').removeClass('indent');
+                	}
+                }
                 return this;
             }
         });
@@ -215,59 +288,59 @@
             }
         });
 
-    });
+		/////-----     ROUTER     -----/////
 
-    /////-----     ROUTER     -----/////
+	    window.App = Backbone.Router.extend({
+	
+	        routes: {
+	            '': 'accounts',
+	            'home': 'home',
+	            'pages': 'pages',
+	            'assets': 'assets',
+	            'accounts': 'accounts',
+	            'services': 'services',
+	            'settings': 'settings'
+	        },
+	
+	        initialize: function () {
+	        	this.controller = window.controller
+				this.appView = new window.AppView({
+					el: $('#app'),
+					controller: this.controller
+				});
+				this.appView.render();
+	        },
+	
+	        home: function () {
+	
+	        },
+	
+	        pages: function () {
+	
+	        },
+	
+	        assets: function () {
+	
+	        },
+	
+	        accounts: function () {
+				this.controller.setState('accounts');
+	        },
+	
+	        services: function () {
+	
+	        },
+	
+	        settings: function () {
+	
+	        }
+	    });
+	
+	    $(function () {
+	        window.App = new window.App();
+	        Backbone.history.start();
+	    });
 
-    window.App = Backbone.Router.extend({
-
-        routes: {
-            '': 'accounts',
-            'home': 'home',
-            'pages': 'pages',
-            'assets': 'assets',
-            'accounts': 'accounts',
-            'services': 'services',
-            'settings': 'settings'
-        },
-
-        initialize: function () {
-        	this.controller = window.controller
-			this.appView = new window.AppView({
-				el: $('#app'),
-				controller: this.controller
-			});
-			this.appView.render();
-        },
-
-        home: function () {
-
-        },
-
-        pages: function () {
-
-        },
-
-        assets: function () {
-
-        },
-
-        accounts: function () {
-			this.controller.setState('accounts');
-        },
-
-        services: function () {
-
-        },
-
-        settings: function () {
-
-        }
-    });
-
-    $(function () {
-        window.App = new window.App();
-        Backbone.history.start();
     });
 
 }(jQuery));
